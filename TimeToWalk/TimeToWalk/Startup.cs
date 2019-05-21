6946ce3ccd74a38cbc3be1901b790313608b33e6
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-05-21 23:02:45 +0300

    initial commit

---
+++ TimeToWalk/TimeToWalk/Startup.cs
@@ -0,0 +1,22 @@
+﻿namespace TimeToWalk
+{
+    using System;
+    public class Startup
+    {
+        public static void Main()
+        {
+            int numberOfSteps = int.Parse(Console.ReadLine());
+            double lentgthInKm = double.Parse(Console.ReadLine()) * numberOfSteps / 1000;
+            double studentSpeedInKm = double.Parse(Console.ReadLine());
+
+            double time = (lentgthInKm / studentSpeedInKm);
+            double rest = lentgthInKm / 0.5;
+            double hours = 0;
+            double minutes = Math.Floor(time * 60);
+            double seconds = Math.Round(((time * 60) - minutes) * 60);
+            minutes = Math.Floor(minutes + rest);
+
+            Console.WriteLine($"{hours}:{minutes}:{seconds}");
+        }
+    }
+}
